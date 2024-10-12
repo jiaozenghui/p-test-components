b51d8f4df9e4129b396918b4262d34da61f07cf7
jzh <11297332+jiaozh1@user.noreply.gitee.com>
2024-10-13 07:39:49 +0800

    fix: fix npm run test bug

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import type { App } from 'vue'
+export { type AllComponentProps } from './defaultProps'
 
 import LText from './components/LText'
 import LImage from './components/LImage'
 import LShape from './components/LShape'
 import FinalPage from './components/FinalPage'
-const components = {LText, LImage, LShape, FinalPage}
+const components = { LText, LImage, LShape, FinalPage }
 
 const install = (app: App) => {
   Object.keys(components).forEach((key) => {
